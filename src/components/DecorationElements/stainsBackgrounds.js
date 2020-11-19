849068578f288ed4bb51652b2605da0e6df5b5a9
Sam S <Saprykinse@gmail.com>
2020-11-19 14:57:31 +0200

    background

--- src/components/DecorationElements/stainsBackgrounds.js
+++ src/components/DecorationElements/stainsBackgrounds.js
@@ -40,7 +40,10 @@ const BackWrapper = styled.div`
        width:100%;
        max-width:800px;
        height: ${props => props.height};
-       
+       @media ${device.desktop} {
+        max-width:600px;
+        height:auto;
+       }
        @media ${device.laptop} {
         max-width:600px;
         height:auto;
@@ -52,9 +55,9 @@ const BackWrapper = styled.div`
        @media ${device.mobileL} {
         max-width:280px;
        }
-       @media ${device.mobile} {
-        max-width:200px;
-      }
+       @media ${device.mobileM} {
+        max-width:280px;
+       }
    }
    z-index:-10;
 `
