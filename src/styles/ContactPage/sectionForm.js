78729e93124d18e7a80225ecb6e32bd9f214ac9e
Sam S <Saprykinse@gmail.com>
2020-11-20 15:28:26 +0200

    contact responsive

--- src/styles/ContactPage/sectionForm.js
+++ src/styles/ContactPage/sectionForm.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { device } from "../constants"
 
 export const WrapperFormSection = styled.div`
     max-width:1002px;
@@ -7,6 +8,19 @@ export const WrapperFormSection = styled.div`
     flex-direction:column;
     color:#E8ECF3;
     position:relative;
+    @media ${device.laptopL} {
+        max-width:960px;
+    }
+    @media ${device.laptop} {
+        max-width:744px;
+    }
+    @media ${device.tablet} {
+        max-width:588px;
+    }
+    @media ${device.mobileL} {
+        
+        padding:0 20px;
+    }
 `
 
 export const ContainerForm = styled.div`
@@ -14,6 +28,9 @@ export const ContainerForm = styled.div`
     flex-direction:row;
     margin:31px 0 150px;
     justify-content:space-between;
+    @media ${device.mobileL} {
+        flex-direction:column;
+    }
 `
 
 export const TitleForm = styled.h5`
@@ -23,13 +40,22 @@ export const TitleForm = styled.h5`
     font-family: Bandeins Sans;
     font-weight:400;
     width:20%;
+    @media ${device.mobileL} {
+        width:100%;
+    }
 `
 
 export const StyledForm = styled.form`
     margin-top:70px;
     width:100%;
     max-width:731px;
-    
+    @media ${device.tablet} {
+        max-width:456px;
+        margin-top:50px;
+    }
+    @media ${device.mobileL} {
+        margin-top:0px;
+    }
 `
 
 export const StyledInput = styled.input`
@@ -44,6 +70,16 @@ export const StyledInput = styled.input`
     font-weight:400;
     caret-color: #1A6AFF;
     margin-top:40px;
+    @media ${device.tablet} {
+        margin-top:0;
+        font-size:34px;
+        line-height:42px;
+    }
+    @media ${device.mobileL} {
+        font-size:24px;
+        line-height:38px;
+        padding-bottom:18px;
+    }
     ::placeholder,
     ::-webkit-input-placeholder {
         font-size:80px;
@@ -52,6 +88,13 @@ export const StyledInput = styled.input`
         opacity:0.4;
         -webkit-text-stroke: 1px #E8ECF3;
         color:transparent;
+        @media ${device.tablet} {
+            font-size:62px;
+        }
+        @media ${device.mobileL} {
+            font-size:42px;
+            line-height:42px;
+        }
     }
 `
 
@@ -63,7 +106,12 @@ export const StyledLabel = styled.label`
     font-family: Bandeins Sans;
     font-weight:400;
     margin-top:70px;
-    
+    @media ${device.tablet} {
+        margin-top:50px;
+    }
+    @media ${device.mobileL} {
+        margin-top:19px;
+    }
 `
 
 export const StyledTextArea = styled.textarea`
@@ -74,11 +122,20 @@ export const StyledTextArea = styled.textarea`
     padding:30px 0;
     width:100%;
     margin-top:40px;
-    caret-color: #1A6AFF
     font-size:24px;
     line-height:34px;
     color:#E8ECF3;
     caret-color: #1A6AFF;
+    @media ${device.tablet} {
+        margin-top:0;
+        font-size:34px;
+        line-height:42px;
+    }
+    @media ${device.mobileL} {
+        font-size:24px;
+        line-height:38px;
+        padding-bottom:18px;
+    }
     ::placeholder,
     ::-webkit-input-placeholder {
         font-size:80px;
@@ -87,6 +144,13 @@ export const StyledTextArea = styled.textarea`
         opacity:0.4;
         -webkit-text-stroke: 1px #E8ECF3;
         color:transparent;
+        @media ${device.tablet} {
+            font-size:62px;
+        }
+        @media ${device.mobileL} {
+            font-size:42px;
+            line-height:42px;
+        }
     }
 `
 
@@ -95,12 +159,22 @@ export const FooterForm = styled.div`
     display:flex;
     flex-direction:row;
     justify-content:space-between;
-    
+    @media ${device.tablet} {
+        margin-top:10px;
+    }
+    @media ${device.mobileL} {
+        flex-direction:column;
+    }
 ` 
 
 
 export const SubmitWrapper = styled.div`
-
+    display:flex;
+    flex-direction:column;
+    @media ${device.tablet} {
+        justify-content:flex-end;
+    }
+    
 `
 
 
@@ -120,6 +194,12 @@ export const CheckLabel = styled.label`
     :hover {
         opacity:0.4;
     }
+    @media ${device.tablet} {
+        width:146px !important;
+        height:48px !important;
+        margin:0 15px 26px 0;
+    }
+    
 `
 
 export const StyledCheckBox = styled.input.attrs({ type: 'checkbox' })`
@@ -154,6 +234,9 @@ export const TitleInputs = styled.h5`
     font-family: Bandeins Sans;
     font-weight:400;
     margin-bottom:32px;
+    @media ${device.tablet} {
+        font-size:18px;
+    }
 `
 
 export const CheckBoxText = styled.h3`
@@ -162,7 +245,9 @@ export const CheckBoxText = styled.h3`
     font-family:Bandeins Sans;
     font-weight:400;
     color: #E8ECF3;
-
+    @media ${device.tablet} {
+        font-size:14px;
+    }
 `
 
 export const ButtonSubmit = styled.input.attrs({ type: 'submit' })`
@@ -178,6 +263,15 @@ export const ButtonSubmit = styled.input.attrs({ type: 'submit' })`
     font-size:20px;
     line-height:34px;
     margin-top:68px;
+    @media ${device.tablet} {
+        width:122px;
+        height:122px;
+        font-size:14px;
+        margin-bottom:26px;
+    }
+    @media ${device.mobileL} {
+        margin:0 auto;
+    }
 `
 
 
@@ -202,7 +296,6 @@ export const FormTextError = styled.div`
       display:block;
       position:absolute;
       transition: all 1s ease-in-out;
-
       font-size:22px;
   }
 `
--- src/styles/ContactPage/sectionHeroCta.js
+++ src/styles/ContactPage/sectionHeroCta.js
@@ -1,12 +1,26 @@
 import styled from "styled-components"
-
+import { device } from "../constants"
 
 export const HeroCtaWrapper = styled.div`
     max-width:1002px;
     margin:0 auto;
-    padding-top:160px;
+    padding-top:230px;
     display:flex;
     flex-direction:column;
+    @media ${device.laptopL} {
+        max-width:960px;
+    }
+    @media ${device.laptop} {
+        max-width:744px;
+        padding-top:170px;
+    }
+    @media ${device.tablet} {
+        max-width:588px;
+        align-items:flex-end;
+    }
+    @media ${device.mobileL} {
+        padding:101px 20px 0;
+    }
 `
 
 export const HeroCtaTitle = styled.div`
@@ -18,6 +32,22 @@ export const HeroCtaTitle = styled.div`
     color:#E8ECF3;
     text-transform:uppercase;
     position:relative;
+    @media ${device.laptopL} {
+        font-size:80px;
+        line-height:96px;
+    }
+    @media ${device.laptop} {
+        font-size:72px;
+        line-height:96px;
+    }
+    @media ${device.tablet} {
+        font-size:46px;
+        line-height:58px;
+    }
+    @media ${device.mobileL} {
+        font-size:38px;
+        line-height:58px;
+    }
     span {
         color:#1A6AFF;
     }
@@ -25,15 +55,53 @@ export const HeroCtaTitle = styled.div`
         padding-left:250px;
         font-size:96px;
         line-height:116px;
-        tont-weight:400;
+        font-weight:400;
         letter-spacing:0.05em;
         color:#E8ECF3;
         font-family:Verona Serial;
+        @media ${device.laptopL} {
+            font-size:80px;
+            line-height:96px;
+        }
+        @media ${device.laptop} {
+            font-size:72px;
+            line-height:96px;
+        }
+        @media ${device.tablet} {
+            font-size:46px;
+            line-height:58px;
+            padding-left:170px;
+        }
+        @media ${device.mobileL} {
+            font-size:38px;
+            line-height:58px;
+            padding-left:96px;
+        }
     }
     svg {
         position:absolute;
         right:30px;
         top:100px;
+        @media ${device.laptopL} {
+            width:234px;
+            right:140px;
+            top:90px;
+        }
+        @media ${device.laptop} {
+            width:212px;
+            right:10px;
+            top:80px;
+        }
+        @media ${device.tablet} {
+            width:133px;
+            top:50px;
+            right:120px;
+        }
+        @media ${device.mobileL} {
+            width:112px;
+            left:0;
+            top:104px;
+        }
     }
 `
 
@@ -47,5 +115,16 @@ export const HeroCtaCta = styled.span`
     margin:0 auto;
     margin-top:-50px;
     padding:0 0 40px 20px;
+    @media ${device.tablet} {
+        font-size:28px;
+        line-height:42px;
+        max-width:398px;
+        margin:50px 0 0;
+        padding-left:0;
+    }
+    @media ${device.mobileL} {
+        font-size:24px;
+        line-height:28px;
+    }
 `  
 
