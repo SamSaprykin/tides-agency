592c0345d83665f8d9fdf9edf7dbd9021d74ec2a
Sam S <Saprykinse@gmail.com>
2020-11-23 11:43:25 +0200

    tablet index test

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+const isBrowser = typeof window !== `undefined`
 export default function useWindowSize() {
+  if (!isBrowser) return { width: 0, height: 0 }
   const isSSR = typeof window !== "undefined";
   const [windowSize, setWindowSize] = React.useState({
     width: isSSR ? 1200 : window.innerWidth,
